1c128dcb56292cc4fca1b960fed9d44136efa6c2
Arthur Teixeira <52799942+arthurtc30@users.noreply.github.com>
2022-07-05 18:29:29 -0300

    Finalizando criação de usuário e login

--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthRequest {
   email: string;
@@ -26,9 +27,25 @@ class AuthUserService {
       throw new Error("Usuário ou senha incorretos");
     }
 
-    // Gerar um token JWT e devolver os dados do usuário como id, name e email
+    // Gerar um token JWT para o usuário
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "30d",
+      }
+    );
 
-    return { ok: true };
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token,
+    };
   }
 }
 
