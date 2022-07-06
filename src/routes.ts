4b19c3560fafdcea294e14a37e175bbb5da54ce1
Arthur Teixeira <52799942+arthurtc30@users.noreply.github.com>
2022-07-06 16:47:39 -0300

    Cadastro de produtos

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,21 @@
 import { Router, Request, Response } from "express";
+import multer from "multer";
+
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import uploadConfig from "./config/multer";
+
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
-import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
+import { CreateProductController } from "./controllers/product/CreateProductController";
+
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
 
 // Rotas User
 router.post("/users", new CreateUserController().handle);
@@ -21,5 +29,13 @@ router.post(
   new CreateCategoryController().handle
 );
 router.get("/category", isAuthenticated, new ListCategoryController().handle);
+
+// Rotas Product
+router.post(
+  "/product",
+  isAuthenticated,
+  upload.single("file"),
+  new CreateProductController().handle
+);
 
 export { router };
