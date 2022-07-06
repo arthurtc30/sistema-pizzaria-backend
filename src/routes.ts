d1b62d6cefa67a33607852fa561f2210eda5e33e
Arthur Teixeira <52799942+arthurtc30@users.noreply.github.com>
2022-07-06 18:15:19 -0300

    Fechar pedido

--- src/routes.ts
+++ src/routes.ts
@@ -21,6 +21,7 @@ import { RemoveItemController } from "./controllers/order/RemoveItemController";
 import { SendOrderController } from "./controllers/order/SendOrderController";
 import { ListOrdersController } from "./controllers/order/ListOrdersController";
 import { DetailOrderController } from "./controllers/order/DetailOrderController";
+import { FinishOrderController } from "./controllers/order/FinishOrderController";
 
 const router = Router();
 
@@ -67,6 +68,11 @@ router.get(
   "/order/details",
   isAuthenticated,
   new DetailOrderController().handle
+);
+router.put(
+  "/order/finish",
+  isAuthenticated,
+  new FinishOrderController().handle
 );
 
 export { router };
